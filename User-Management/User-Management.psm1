e72408ddbb882d8d3187b458fe480d9d04c6ccbe
Zachary Hilliker <zhilliker@rhythmedix.com>
2020-09-06 16:10:06 -0400

    new-shorturl fix

--- User-Management/User-Management.psm1
+++ User-Management/User-Management.psm1
@@ -971,9 +971,9 @@ function New-WVDRemoteApp{
           )
         if($alias -eq "")
         {
-        invoke-WebRequest -UseBasicParsing https://rmx.health/ShortenUrl?code=7rxIKBuRNQ8fQBPvHgdH52jYgdupvmHLbLRrTtheyzJ9/Wr/1LaKvw== -ContentType "application/json" -Method POST -Body "{ 'longUrl':$longurl}"
+        invoke-WebRequest -UseBasicParsing https://rmx.health/ShortenUrl?code=7rxIKBuRNQ8fQBPvHgdH52jYgdupvmHLbLRrTtheyzJ9/Wr/1LaKvw== -ContentType "application/json" -Method POST -Body "{ 'longUrl':`'$longurl`'}"
         }
         else {
-            invoke-WebRequest -UseBasicParsing https://rmx.health/ShortenUrl?code=7rxIKBuRNQ8fQBPvHgdH52jYgdupvmHLbLRrTtheyzJ9/Wr/1LaKvw== -ContentType "application/json" -Method POST -Body "{ 'longUrl':$longurl, 'alias':$alias}"
+            invoke-WebRequest -UseBasicParsing https://rmx.health/ShortenUrl?code=7rxIKBuRNQ8fQBPvHgdH52jYgdupvmHLbLRrTtheyzJ9/Wr/1LaKvw== -ContentType "application/json" -Method POST -Body "{ 'longUrl':`'$longurl`', 'alias':`'$alias`'}"
         }
       }
